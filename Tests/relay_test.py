ec4d120f0937a74f5cba061bc463699e78fa2ae1
Jorge L. Alberto <jorgealberto@ufl.edu>
2025-04-18 10:32:16 -0400

    relay test for input_gain again

--- Tests/relay_test.py
+++ Tests/relay_test.py
@@ -5,7 +5,7 @@ import time
 
 relays =  Relays()
 
-resistor_values = ['100', '1k', '10k', '50k', '100k', '500k', '1Meg', '10Meg', 'Randles', 'Counter']
+resistor_values = ['10x', '100x', '1kx', '10kx', '100kx', '1Megx']
 for res in resistor_values:
     print(f"setting calibration to {res}")
     relays.set_input_gain(res)
