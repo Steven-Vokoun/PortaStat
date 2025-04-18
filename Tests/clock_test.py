f8c4865f2507cd5afe8ba22309287a1116367286
Steven Vokoun <stevenvokoun@gmail.com>
2025-04-18 19:32:21 -0400

    changed clock_test

--- Tests/clock_test.py
+++ Tests/clock_test.py
@@ -1,11 +1,12 @@
+import sys
+sys.path.append('../')
 from Libraries.MUX_and_CLK_Library import LTC6904
 import numpy as np
 import time
 
 clk =  LTC6904()
 
-freqs = np.linspace(10_000, 16.776e3, 100)
+freqs = np.linspace(10_000, 20_000, 30_000, 100_000)
 for freq in freqs:
-    sys_clk = min(freq * 1_000, 16.776e6)
     clk.Turn_On_Clock(freq)
-    time.sleep(30)
+    time.sleep(15)
