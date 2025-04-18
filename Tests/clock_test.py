631f66274add5af6a132786d3c20b13a1bd87df8
Steven Vokoun <stevenvokoun@gmail.com>
2025-04-18 19:57:25 -0400

    fixed clock library

--- Tests/clock_test.py
+++ Tests/clock_test.py
@@ -6,7 +6,7 @@ import time
 
 clk =  LTC6904()
 
-freqs = np.linspace(10_000, 20_000, 30_000, 100_000)
+freqs = np.linspace(50_000, 20_000, 30_000, 100_000)
 for freq in freqs:
     clk.Turn_On_Clock(freq)
     time.sleep(15)
